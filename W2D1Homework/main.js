c4c2d183abae35ec27853e293f2be8ec7b261889
Charmel Sippio <charmelsippio@gmail.com>
2016-06-15 15:45:49 -0400

    Homework Resubmit

--- W2D1Homework/main.js
+++ W2D1Homework/main.js
@@ -2,14 +2,14 @@
 var num1 = 10;
 var num2 = 23;
 function adding() {
-	return num1 + num2;
+	alert(num1 + num2);
 }
 adding();
 
 var string1 = "Four for you, Glen Coco!";
 var string2 = "You go, Glen Coco!"
 function strings() {
-	return string1 + " " + string2;
+	alert(string1 + " " + string2);
 }
 strings();
 //I realized I am having difficulty when it comes to combining function with var and how those two things can be nested together, how to properly call and keep track of one over the other, and how to ensure syntax stays intact. 
@@ -17,12 +17,13 @@ strings();
 //DRY version Problem 1
 //I'm not sure how I would fit "console.log" to get this to return a figure w/o having to call the function.
 function adding1 (num1, num2) {
-	return num1 + num2;
+	document.write(num1 + num2);
+	console.log(num1 + num2);
 }
 adding1(3, 4);
 
 function strings1 (strng1, strng2) {
-	return strng1 + " " + strng2;
+	console.log(strng1 + " " + strng2);
 }
 
 strings1("Four for you Glen Coco!", "You go, Glen Coco!");
@@ -38,9 +39,9 @@ console.log(tennis.slice(0, 3));
 //Problem 3
 function lessThan() {
 	if (lessThan > 10) {
-		return "Your number is less than 10";
+		alert("Your number is less than 10");
 	} else {
-		return "Your number is greater than 10";
+		alert("Your number is greater than 10");
 	}
 }
 lessThan(20);
@@ -52,7 +53,8 @@ lessThan(20);
 //Problem 6
 function enterName() {
 	var name = prompt ("Enter your name in the box");
-	confirm ("Your name was entered");
+	confirm ("Your name, " + name + ", was entered");
+	console.log(name);
 }
 enterName();
 //I wanted to get this to return in the confirm box the person's name, but I couldn't figure out how to do that. 
@@ -61,13 +63,16 @@ enterName();
 
 //Optional Problem
 var userPicks = prompt ("Enter Door 1, Door 2, Door 3");
-var theDoors = function (door1, door2, door3) {
-	if (theDoors = "Door 1") {
+var theDoors = function () {
+	if (userPicks == "Door 1") {
 		alert ("You win a 1972 Chevy Nova!");
 	}
-	else if (theDoors = "Door 2") {
+	else if (userPicks == "Door 2") {
 		alert ("You win a big glass of air!");
-	} else { theDoors = "Door 3"; }
-		alert ("You win an all-expenses-paid trip to North Korea!! Congrats, Commrade!")
-	};
+	} 
+	else if (userPicks == "Door 3") {
+		alert ("You win an all-expenses-paid trip to North Korea!! Congrats, Commrade!");
+	}
+};
+theDoors();
 //I am not sure why, once a door is entered, the alert is not popping up for re: the corresponding door. 
